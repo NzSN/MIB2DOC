fbce0787c345a5ee42ff004e6b5766d6f1b75ed3
Tootooroo <aydenlin@foxmail.com>
2019-08-13 23:30:34 +0800

    add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((nil . ((company-clang-arguments . ("-I/home/aydenlin/Projects/MIB2DOC/include/"
+                                     "-I/home/aydenlin/Projects/MIB2DOC/include/datastructures/")))))
